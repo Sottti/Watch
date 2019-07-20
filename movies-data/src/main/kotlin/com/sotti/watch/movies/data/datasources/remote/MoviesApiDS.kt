7dc600cffa4ca1108f98d9875b40f252b089bc56
Pablo <sottti@gmail.com>
2019-07-20 14:17:26 +0100

    Fixes a bug where the exceptions launched by Retrofit weren't captured.

--- movies-data/src/main/kotlin/com/sotti/watch/movies/data/datasources/remote/MoviesApiDS.kt
+++ movies-data/src/main/kotlin/com/sotti/watch/movies/data/datasources/remote/MoviesApiDS.kt
@@ -3,20 +3,23 @@ package com.sotti.watch.movies.data.datasources.remote
 import com.sotti.watch.movies.data.datasources.MoviesDataSources
 import com.sotti.watch.movies.data.datasources.toDM
 import com.sotti.watch.movies.domain.ErrorLoadingMoviesDM
-import com.sotti.watch.movies.domain.LoadMoviesResultDM
 import com.sotti.watch.movies.domain.NoMoviesFoundDM
 import com.sotti.watch.movies.domain.SuccessLoadingMoviesDM
 
 internal class MoviesApiDS(private val service: TmdbApiService) : MoviesDataSources.RemoteDS {
 
-    override suspend fun loadPopularMovies(): LoadMoviesResultDM =
-        with(service.loadPopularMovies()) {
-            val response = body()
-            if (isSuccessful && response != null) {
-                if (response.hasMovies()) SuccessLoadingMoviesDM(
-                    response.results.toDM()
-                )
-                else NoMoviesFoundDM
-            } else ErrorLoadingMoviesDM
+    override suspend fun loadPopularMovies() =
+        try {
+            with(service.loadPopularMovies()) {
+                val response = body()
+                if (isSuccessful && response != null) {
+                    if (response.hasMovies()) SuccessLoadingMoviesDM(
+                        response.results.toDM()
+                    )
+                    else NoMoviesFoundDM
+                } else ErrorLoadingMoviesDM
+            }
+        } catch (e: Exception) {
+            ErrorLoadingMoviesDM
         }
 }
