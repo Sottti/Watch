f8f037a1a5682175b2f16efa942889a4490fb756
Pablo <sottti@gmail.com>
2019-07-28 07:55:52 +0100

    Migrated ui-automator from the support library version to the Androidx version.

--- android-tests-common/src/main/kotlin/com/sotti/watch/android/tests/common/DisableAnimationsRule.kt
+++ android-tests-common/src/main/kotlin/com/sotti/watch/android/tests/common/DisableAnimationsRule.kt
@@ -26,8 +26,8 @@ class DisableAnimationsRule : TestRule {
     private fun disableAnimations() {
         with(UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())) {
             executeShellCommand("settings put global transition_animation_scale 0")
-            executeShellCommand("settings put global window_animation_scale 0")
             executeShellCommand("settings put global animator_duration_scale 0")
+            executeShellCommand("settings put global window_animation_scale 0")
         }
     }
 
@@ -35,8 +35,8 @@ class DisableAnimationsRule : TestRule {
     private fun enableAnimations() {
         with(UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())) {
             executeShellCommand("settings put global transition_animation_scale 1")
-            executeShellCommand("settings put global window_animation_scale 1")
             executeShellCommand("settings put global animator_duration_scale 1")
+            executeShellCommand("settings put global window_animation_scale 1")
         }
     }
 }
